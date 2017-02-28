41b5e6eba232002a7dc779da56aff12321d4a2e6
Dan Park <danielpark.487@gmail.com>
2017-02-28 16:27:03 -0500

    feat(orders): changed price gettermethod in lineitems model

--- db/models/lineitem.js
+++ db/models/lineitem.js
@@ -1,19 +1,17 @@
 const Sequelize = require('sequelize');
 const db = require('APP/db');
 
-const Product = require('APP/db/product');
-
 const LineItem = db.define('LineItems', {
     quantity: {
+        type: Sequelize.INTEGER
+    },
+    price: {
         type: Sequelize.INTEGER
     }
 }, {
     getterMethods: {
-        price: function () {
-            Product.findById(this.productId)
-                .then((product) => {
-                    return product.price * this.quantity;
-                })
+        totalPrice: function () {
+            return this.quantity * this.price;
         }
     }
 });

f0a7b7171e6ee0b50ca40d2ba74a01e8f6ec00e7
Justin Zhou <justinzhou93@gmail.com>
2017-02-28 14:13:11 -0500

    put in a few new files for backend structure

---
+++ server/api/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router()
+
+router.use('/users', require('./users'))
+router.use('/products', require('./products'))
+router.use('/orders', require('./orders'))
+
+module.exports = router

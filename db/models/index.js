cb25e61099305476eb2955633e9eb8bb5e7254da
Zohaib Rahman <zohaibr@gmail.com>
2017-02-28 16:58:54 -0500

    feat(associations): Added All the associations in the index file (#18)

--- db/models/index.js
+++ db/models/index.js
@@ -9,11 +9,26 @@ const OAuth = require('./oauth');
 const CreditCard = require('./creditcard');
 const ShippingAddress = require('./shippingaddress');
 const BillingAddress = require('./billingaddress');
+const Product = require('./product');
+const Review = require('./review');
+const Category = require('./category');
+const Cart = require('./cart');
+const LineItem = require('./lineitem');
+const Order = require('./order');
 
 OAuth.belongsTo(User);
 User.hasOne(OAuth);
 User.hasMany(CreditCard);
 User.hasMany(ShippingAddress);
 User.hasMany(BillingAddress);
+Review.belongsTo(Product);
+Review.belongsTo(User);
+Product.belongsToMany(Category, {through: 'ProductCategory'});
+Category.belongsToMany(Product, {through: 'ProductCategory'});
+Order.belongsTo(User);
+LineItem.belongsTo(Order);
+LineItem.belongsTo(Product);
+Cart.belongsTo(User);
+Cart.belongsTo(Product);
 
-module.exports = {User, CreditCard, ShippingAddress, BillingAddress};
+module.exports = {User, CreditCard, ShippingAddress, BillingAddress, Product, Review, Category, Cart, LineItem, Order};

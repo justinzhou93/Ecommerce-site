7976f348ee9b0d11b33ab15527f4c3b5f8982e40
Justin Zhou <justinzhou93@gmail.com>
2017-03-01 16:18:33 -0500

    fixed routes

--- server/api.js
+++ server/api.js
@@ -7,6 +7,8 @@ api
   .get('/heartbeat', (req, res) => res.send({ok: true}))
   .use('/auth', require('./api/auth'))
   .use('/users', require('./api/users'))
+  .use('/products', require('./api/products'))
+  .use('/orders', require('./api/orders'))
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())

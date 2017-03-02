b55ecf535b0febabf829e65201cc40b3072d0c7c
danpark487 <danielpark.487@gmail.com>
2017-03-02 12:52:59 -0500

    Orderroutes (#39)
    * added order routes
    
    * added an put request for orders
    
    * updated some cart definitions and order routes
    
    * feat(a lot of stuff): order routes, user routes, user models, order models, line item models, removed cart model

--- db/models/order.js
+++ db/models/order.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('APP/db');
 
-const Order = db.define('Order', {
+const Order = db.define('orders', {
     status: {
         type: Sequelize.ENUM('Created', 'Processing', 'Cancelled', 'Completed'),
         defaultValue: 'Created'

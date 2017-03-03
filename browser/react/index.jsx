3ad7a3d8a8d7cd055cbb9648da99caf2fc1cabbc
Justin Zhou <justinzhou93@gmail.com>
2017-03-02 20:09:41 -0500

    feat(react): react routes and other setup

--- browser/react/index.jsx
+++ browser/react/index.jsx
@@ -1,1 +1,12 @@
-// where you put ReactDOM.render...
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store';
+import Routes from './routes';
+
+ReactDOM.render(
+  <Provider store = {store} >
+    <Routes />
+  </Provider>,
+  document.getElementById('app')
+);

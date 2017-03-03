2a18267b9848a7dfb1cc7c20c4d17dd51662be1c
Dan Park <danielpark.487@gmail.com>
2017-03-02 20:47:48 -0500

    added products action creator and reducer

---
+++ browser/react/action-creators/products.jsx
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+/** Constants */
+export const SET_PRODUCT_LIST = 'SET_PRODUCT_LIST';
+export const SET_SINGLE_PRODUCT = 'SET_SINGLE_PRODUCT';
+export const REMOVE_SINGLE_PRODUCT = 'REMOVE_SINGLE_PRODUCT';
+
+/** Action-creators */
+const settingProductList = (products) => {
+    return {
+        type: SET_PRODUCT_LIST,
+        productList: products
+    }
+}
+
+const setSingleProduct = (product) => {
+    return {
+        type: SET_SINGLE_PRODUCT,
+        currentProduct: product
+    }
+}
+
+const removeProduct = () => {
+    return {
+        type: REMOVE_SINGLE_PRODUCT
+    }
+}
+
+/** Thunk actions */
+
+// load all products
+export const loadAllProducts = () => {
+    return dispatch => {
+        axios.get('/api/products')
+            .then((products) => {
+                dispatch(settingProductList(products));
+            });
+    }
+};
+
+
+// loads single product
+export const loadSingleProduct = (productId) => {
+    return dispatch => {
+        axios.get(`/api/products/${productId}`)
+            .then((product) => {
+                dispatch(setSingleProduct(product));
+            });
+    }
+}
+
+/** ADMIN thunks */
+
+
+

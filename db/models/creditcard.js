24d47aa8bd05d8a5cbdfed689b7d4a383f2b7c7a
Benjamin <benjamin.gu100@gmail.com>
2017-03-04 18:59:21 -0500

    feat(TestsModel): built test models for address, category, creditcard, working on line item and added to creditcard model

--- db/models/creditcard.js
+++ db/models/creditcard.js
@@ -5,6 +5,22 @@ const CreditCard = db.define('credit_cards', {
   number: {
     type: Sequelize.INTEGER,
     allowNull: false
+  },
+  name: {
+  	type: Sequelize.STRING,
+  	allowNull: false
+  },
+  month: {
+  	type: Sequelize.INTEGER,
+  	allowNull: false
+  },
+  year: {
+  	type: Sequelize.INTEGER,
+  	allowNull: false
+  },
+  CCV: {
+  	type: Sequelize.INTEGER,
+  	allowNull: false
   }
 });
 

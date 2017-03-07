dad82ce9e6251448ab1c36a33f9072eaa7bbe1d2
Justin Zhou <justinzhou93@gmail.com>
2017-03-06 20:16:43 -0500

    added reviews for product

--- browser/react/components/SingleProduct.jsx
+++ browser/react/components/SingleProduct.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+// import Star from 'react-star-rating';
 
 export default function (props) {
     if (props.currentProduct) {
+        const productReviews = props.currentProduct.reviews;
         return (
             <div className="sing-product-container">
                 <div className="sing-product-img-container">
@@ -16,7 +18,23 @@ export default function (props) {
                     </div>
                 </div>
                 <div className="sing-product-reviews">
-                    <h5>Reviews would go here</h5>
+                    <div className = "reviews-title">
+                      <h1>REVIEWS</h1>
+                    </div>
+                      {
+                        // Each individual review
+                        productReviews.map(review => {
+                          return (
+                            <div key={review.id} className = "sing-product-desc">
+                              <h2>{review.title}</h2>
+                              {
+                                // <Star totalStars={review.rating} />
+                              }
+                              <p>{review.body}</p>
+                            </div>
+                          )
+                        })
+                      }
                 </div>
             </div>
         );

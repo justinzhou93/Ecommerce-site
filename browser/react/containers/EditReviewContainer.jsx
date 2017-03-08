a836a7a24852db1381eedb039658c605e77e9c6d
Dan Park <danielpark.487@gmail.com>
2017-03-08 12:17:16 -0500

    added a filter bar

--- browser/react/containers/EditReviewContainer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import EditReview from '../components/EditReview';
-
-const mapStateToProps = state => ({currentUser: state.auth.currentUser});
-
-export default connect(mapStateToProps)(EditReview);
